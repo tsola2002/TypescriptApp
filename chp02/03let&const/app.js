a0212f5f68f290f74ec897ad27305a140eeca572
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2020-01-23 05:53:54 +0100

    using let & const keyword

---
+++ chp02/03let&const/app.js
@@ -0,0 +1,11 @@
+var container = document.getElementById('container');
+for (var x = 0; x <= 5; x++) {
+    // lets you define variables
+    // you can use to replace the var keyword
+    // let counter = x;
+    // const stands for constant which lets you define a variable that cannot be changed
+    var counter = x;
+    // const cannot be reassigned because its a constant
+    //counter = 1;
+}
+console.log(counter);
